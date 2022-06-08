fed6e2e85df7a0f699c83537eda9bee5feff5e81
benrotlevy <benjamin@rotlevy.com>
2022-06-08 12:12:50 +0300

    add search

--- src/components/guests/Guests.js
+++ src/components/guests/Guests.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { guestsAPI } from "../../api/api";
 import { AddGuest } from "../addGuest/AddGuest";
 import Row from "../row/Row";
@@ -7,18 +7,37 @@ import  "./guests.css";
 const Guests = () => {
     
     const [guestsList, setGuestsList] = useState([]);
+    const [guestsListToDisplay, setGuestsListToDisplay] = useState([]);
+    const [searchInput, setSearchInput] = useState("");
+
+    const searchInputRef = useRef("");
 
     useEffect(() => {
         const getData = async() => {
             try {
                 const {data} = await guestsAPI.get("/");
                 setGuestsList(data);
+                setGuestsListToDisplay(data);
             } catch (error) {
                 console.log(error);
             }
         }
         getData();
     },[])
+
+    useEffect(()=> {
+        setGuestsListToDisplay([...guestsList]);
+    }, [guestsList])
+
+    useEffect(()=> {
+        if(searchInputRef.current !== searchInput) {
+            setGuestsListToDisplay(guestsList.filter(guest => {
+                const guestToString = guest.firstName+ " " +guest.lastName+" " +guest.label;
+                return guestToString.includes(searchInput);
+            }))
+        }
+        searchInputRef.current = searchInput;
+    }, [searchInput, guestsListToDisplay])
 
     const removeGuest= (id) => {
         setGuestsList(prev => prev.filter(guest => guest.id !== id));
@@ -29,14 +48,17 @@ const Guests = () => {
     }
 
     const drawTable = () => {
-        return guestsList.map(guest => {
+        return guestsListToDisplay.map(guest => {
             return <Row key={guest.id} content={guest} remove={removeGuest}/>
         })
     }
 
     return (
         <div>
-            {/* {console.log(guestsList)} */}
+            <div className="search">
+                {/* Search:  */}
+                <input value={searchInput} onChange={(e)=> setSearchInput(e.target.value)} />
+            </div>
             <AddGuest add={addGuest}/>
             {drawTable()}
         </div>
